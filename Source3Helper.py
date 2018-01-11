8b7809990fa3914db394ed7ecf3c18e761edd63a
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2018-01-11 21:40:15 +0530

    update source 3 helper

--- Source3Helper.py
+++ Source3Helper.py
@@ -2,7 +2,7 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
-def get_data_from_source3():
+def get_data_from_source3(lang):
 
     source_url = "https://medium.com/web-development-zone/a-complete-list-of-computer-programming-languages-1d8bc5a891f"
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -10,22 +10,28 @@ def get_data_from_source3():
     page = urllib2.urlopen(req)
     soup = BeautifulSoup(page, "html.parser")
 
-    paradigm_dict = {}
+    lang_dict = {}
     tag = soup.find('h3')
-    cur_paradigm = tag.get_text()
-    cur_lang_list = []
+    cur_paradigm = str(tag.get_text())
 
     while tag.find_next_sibling('h3') is not None:
         while tag.next_sibling.name != "h3":
             tag = tag.next_sibling
             if tag.name == "h4":
-                cur_lang_list.append(tag.get_text())
-        paradigm_dict[cur_paradigm] = cur_lang_list
+                cur_lang = tag.get_text()
+                if cur_lang in lang_dict:
+                    lang_dict[cur_lang].append(cur_paradigm)
+                else:
+                    lang_dict[cur_lang] = [cur_paradigm]
         tag = tag.next_sibling
-        cur_paradigm = tag.get_text()
-        cur_lang_list = []
+        cur_paradigm = str(tag.get_text())
 
     lang_list = tag.find_next_siblings("h4")
     for l in lang_list:
-        cur_lang_list.append(l.get_text())
-    paradigm_dict[cur_paradigm] = cur_lang_list
+        cur_lang = l.get_text()
+        if cur_lang in lang_dict:
+            lang_dict[cur_lang].append(cur_paradigm)
+        else:
+            lang_dict[cur_lang] = [cur_paradigm]
+
+
